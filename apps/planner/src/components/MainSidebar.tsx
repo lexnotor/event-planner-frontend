6596fcf4a42e89c5d83a129324cf9789382fa027
alexandre-notor <amanichambu48@gmail.com>
2023-06-18 08:52:18 +0200

    feat: setup sidebar

--- apps/planner/src/components/MainSidebar.tsx
+++ apps/planner/src/components/MainSidebar.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Button } from "ui";
 
 const MainSidebar = () => {
-    return <div>MainSidebar</div>;
+    return (
+        <div className="p-4">
+            <nav className="flex flex-col gap-4">
+                <Button>Zert</Button>
+                <Button active>Zert</Button>
+                <Button>Zert</Button>
+                <Button>Zert</Button>
+            </nav>
+        </div>
+    );
 };
 
 export default MainSidebar;
