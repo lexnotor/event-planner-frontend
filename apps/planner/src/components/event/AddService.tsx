b1c2f54b0dd85279117325040ac85089eafb69a5
alexandre-notor <amanichambu48@gmail.com>
2023-08-15 13:56:37 +0200

    feat: add search for service

--- apps/planner/src/components/event/AddService.tsx
+++ apps/planner/src/components/event/AddService.tsx
@@ -6,22 +6,42 @@ import { useNewEventContext } from "./context/NewEventContext";
 const AddService = () => {
     const context = useNewEventContext();
 
-    const submit = () => {
-        context.addService({ details: "", supplier: "", type: "LAUNCH" });
+    const submit = (type: (typeof context.services)[number]["type"]) => {
+        context.addService({ details: "", supplier: "", type });
     };
 
     return (
-        <div className="flex gap-2 flex-wrap" onClick={submit}>
-            <Button size="small">+ Repas</Button>
-            <Button size="small">+ Animation</Button>
-            <Button size="small">+ Décoration</Button>
-            <Button size="small">+ Sonorisation</Button>
-            <Button size="small">+ Salle</Button>
-            <Button size="small">+ Organisateur</Button>
-            <Button size="small">+ Transport</Button>
-            <Button size="small">+ Habillement</Button>
-            <Button size="small">+ Imprimerie</Button>
-            <Button size="small">+ Location</Button>
+        <div className="flex gap-2 flex-wrap">
+            <Button size="small" onClick={() => submit("LAUNCH")}>
+                + Repas
+            </Button>
+            <Button size="small" onClick={() => submit("MC")}>
+                + Animation
+            </Button>
+            <Button size="small" onClick={() => submit("DECORATION")}>
+                + Décoration
+            </Button>
+            <Button size="small" onClick={() => submit("SONORISATION")}>
+                + Sonorisation
+            </Button>
+            <Button size="small" onClick={() => submit("ROOM")}>
+                + Salle
+            </Button>
+            <Button size="small" onClick={() => submit("ORGANISATEUR")}>
+                + Organisateur
+            </Button>
+            <Button size="small" onClick={() => submit("TRANSPORT")}>
+                + Transport
+            </Button>
+            <Button size="small" onClick={() => submit("HABILLEMENT")}>
+                + Habillement
+            </Button>
+            <Button size="small" onClick={() => submit("IMPRIMERIE")}>
+                + Imprimerie
+            </Button>
+            <Button size="small" onClick={() => submit("LOCATION")}>
+                + Location
+            </Button>
         </div>
     );
 };
--- apps/planner/src/components/event/services/LaunchServices.tsx
+++ apps/planner/src/components/event/services/LaunchServices.tsx
@@ -1,5 +1,7 @@
 "use client";
-import React from "react";
+import useToggle from "@/hooks/toggle";
+import { Modal } from "antd";
+import { CiSearch } from "react-icons/ci";
 import { Button } from "ui";
 import { useNewEventContext } from "../context/NewEventContext";
 
@@ -7,46 +9,66 @@ const LaunchServices = ({ id }: { id: string }) => {
     const context = useNewEventContext();
     const current = context.services.find((item) => item.id == id);
 
+    const [isSearch, setIsSearching] = useToggle(false);
+
     return (
-        <div className="flex flex-col gap-2 max-w-md w-full bg-white duration-500 transition-colors p-4 rounded-md border border-primary-500">
-            <h3>{current?.type ?? "INCONNUE"}</h3>
-            <hr />
-            <table>
-                <tbody>
-                    <tr>
-                        <td>
-                            <label htmlFor="">Prestataire :</label>
-                        </td>
-                        <td>
-                            <input
-                                type="text"
-                                placeholder="PREMIERE DAME"
-                                defaultValue={current?.supplier ?? ""}
-                                className="w-[20rem] border rounded-l-full rounded-r-full border-primary-900 bg-primary-200 px-4 py-2 focus:outline-none bg-transparent"
-                            />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label htmlFor="">Détails :</label>
-                        </td>
-                        <td>
-                            <input
-                                type="text"
-                                placeholder="260 Invités; 260 + 20 plats"
-                                defaultValue={current?.details ?? ""}
-                                className="w-[20rem] border rounded-l-full rounded-r-full border-primary-900 bg-primary-200 px-4 py-2 focus:outline-none bg-transparent"
-                            />
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <div className="flex justify-end">
-                <Button size="small" onClick={() => context.deleteService(id)}>
-                    Supprimer
-                </Button>
+        <>
+            <div className="flex flex-col gap-2 max-w-md w-full bg-white duration-500 transition-colors p-4 rounded-md border border-primary-500">
+                <h3>{current?.type ?? "INCONNUE"}</h3>
+                <hr />
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <label htmlFor="">Prestataire :</label>
+                            </td>
+                            <td className="flex w-[20rem] gap-2 items-center">
+                                <input
+                                    type="text"
+                                    placeholder="PREMIERE DAME"
+                                    defaultValue={current?.supplier ?? ""}
+                                    className="grow border rounded-l-full rounded-r-full border-primary-900 bg-primary-200 px-4 py-2 focus:outline-none bg-transparent"
+                                />
+                                <span
+                                    className="text-3xl cursor-pointer hover:text-blue-700"
+                                    onClick={() => setIsSearching(true)}
+                                >
+                                    <CiSearch />
+                                </span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label htmlFor="">Détails :</label>
+                            </td>
+                            <td className="flex w-[20rem]">
+                                <input
+                                    type="text"
+                                    placeholder="260 Invités; 260 + 20 plats"
+                                    defaultValue={current?.details ?? ""}
+                                    className="grow border rounded-l-full rounded-r-full border-primary-900 bg-primary-200 px-4 py-2 focus:outline-none bg-transparent"
+                                />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div className="flex justify-end">
+                    <Button
+                        size="small"
+                        onClick={() => context.deleteService(id)}
+                    >
+                        Supprimer
+                    </Button>
+                </div>
             </div>
-        </div>
+            <Modal
+                destroyOnClose
+                open={isSearch}
+                closable
+                footer={null}
+                onCancel={() => setIsSearching(false)}
+            />
+        </>
     );
 };
 
